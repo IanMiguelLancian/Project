6048a88cadf8d6e01069cd4e8b29858e170e7cb5
IanMiguelLancian <imLancian@mcm.edu.ph>
2021-11-12 11:15:36 +0800

    Update dashboard.dart

--- project/lib/dashboard.dart
+++ project/lib/dashboard.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:project/splash.dart';
 import 'bg.dart';
 import 'works.dart';
+import 'ly.dart';
 
 class dashboard extends StatefulWidget {
   const dashboard({Key? key}) : super(key: key);
@@ -21,6 +22,10 @@ class _dashboardState extends State<dashboard> {
     Navigator.pushReplacement(
       context, MaterialPageRoute(builder: (context) => works()));
   }
+  navigatetopushly(){
+    Navigator.pushReplacement(
+      context, MaterialPageRoute(builder: (context) => ly()));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,19 +39,18 @@ class _dashboardState extends State<dashboard> {
       ),
       backgroundColor: Colors.white,
       body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset(
-                  'pics/fne.jpg',
-                  height: 400,
-                  width: 300,
-                  color: Colors.white,
-                  colorBlendMode: BlendMode.darken,
-                  fit: BoxFit.fitWidth,
-                ),
+        padding: EdgeInsets.all(25),
+        child: Column(children: [
+          Container(
+            height: 380,
+            width: 600,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+            child: Image.asset(
+              'pics/fne.jpg'),
+            ),
+                Padding(padding: EdgeInsets.all(10)),
                 Container(
                   clipBehavior: Clip.antiAliasWithSaveLayer,
                   width: double.infinity,
@@ -92,12 +96,33 @@ class _dashboardState extends State<dashboard> {
                     ),
                   ),
                  ),
+                  SizedBox(
+                  height: 10,
+                  ),
+                  Container(
+                  clipBehavior: Clip.antiAliasWithSaveLayer,
+                  width: double.infinity,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  child: MaterialButton(
+                    onPressed: (){
+                      navigatetopushly();
+                    },
+                    color: Colors.brown[300],
+                    child: Text(
+                      "Nietzsche's later years",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                 ),
               ]),
           ),
-        ),
-      ),
     );
-  
   }
 }
 
